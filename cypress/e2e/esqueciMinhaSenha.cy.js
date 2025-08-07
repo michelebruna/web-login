0d6f5d8715994aa6291b93331ca7ae46d8126886
Michele <michelebrunasilva@gmail.com>
2025-08-06 21:17:53 -0300

    Adicionando o bloqueio de senha

--- cypress/e2e/esqueciMinhaSenha.cy.js
+++ cypress/e2e/esqueciMinhaSenha.cy.js
@@ -3,14 +3,40 @@ describe('Esqueci minha senha', () => {
       cy.visit('/')
     })
 
-    it('Deve retornar sucesso com 201 quando o usuário informado for válido', () => {
-        cy.get('#forgot-password-link').click()
-        cy.get('#forgot-username').click({ force: true }).type('grupo4')
-        cy.contains('button', 'Gerar Token').click()
-        
-        cy.contains('div', 'Token gerado com sucesso!').should('be.visible')
-        
+    it('Deve retornar Token gerado com sucesso quando o usuário informado for válido', () => {
+        let token
+
+      cy.get('#forgot-password-link').click()
+      cy.get('#forgot-username').click({ force: true }).type('grupo4')
+      cy.contains('button', 'Gerar Token').click()
+
+      cy.contains('div', 'Token gerado com sucesso!').should('be.visible')
+
+        l
+     //cy.get('#copy-token').click()
     })
+
+    //it('')
     
+})
+
+describe('Resetar senha', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('Deve salvar a nova senha quando informar um token válido', () => {
+    cy.get('#go-to-reset').click()
+    cy.get('#reset-username').click().type('grupo4')
+    cy.get('#reset-token').click().type(token)
+    cy.get('#new-password').click().type('123456')
+    cy.get('#confirm-password').click().type('123456')
+  
+  
+  
+
+        
+
+})
 })
   
