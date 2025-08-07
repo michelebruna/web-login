43769420d94c3dd482166fdc2ba547f55bc549a6
Michele <michelebrunasilva@gmail.com>
2025-08-07 15:32:24 -0300

    Incluindo fixtures nos testes do login.cy.js

--- cypress/e2e/esqueciMinhaSenha.cy.js
+++ cypress/e2e/esqueciMinhaSenha.cy.js
@@ -15,10 +15,10 @@ describe('Esqueci minha senha', () => {
       cy.get('#generated-token')
   .invoke('val')
   .then((tokenGerado) => {
-    // Clica para ir até a tela de redefinir senha
+  
     cy.get('#go-to-reset').click({ force: true })
     
-    // Preenche os campos de redefinição com o token capturado
+    
     cy.get('#reset-username').click({ force: true }).type('grupo4')
     cy.get('#reset-token').click({ force: true }).type(tokenGerado)
     cy.get('#new-password').click({ force: true }).type('123456')
